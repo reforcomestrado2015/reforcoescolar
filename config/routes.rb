6e0c0c5e13a35afc52bdbf86de50982bb97fc21f
jose henrique davino <jhmd@cin.ufpe.br>
2015-06-19 14:18:36 +0000

    add crud professor, aluno, permissaoacesso e disciplina

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :permissaoacessos
+  resources :disciplinas
+  resources :alunos
+  resources :professors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
